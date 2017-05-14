468de02370ba37c454683288d4d096242bc5e9b1
iray-tno <iray.tno@gmail.com>
2017-05-14 09:48:20 +0900

    Refactor Page layout with my flavor

--- src/layouts/Page/index.js
+++ src/layouts/Page/index.js
@@ -1,97 +1,100 @@
-import React, { PropTypes } from "react"
-import Helmet from "react-helmet"
-import warning from "warning"
-import { BodyContainer, joinUri, Link } from "phenomic"
+import React, { PropTypes } from 'react';
+import Helmet from 'react-helmet';
+import warning from 'warning';
+import { BodyContainer, joinUri, Link } from 'phenomic';
 
-import Button from "../../components/Button"
-import Loading from "../../components/Loading"
+import Button from '../../components/Button';
+import Loading from '../../components/Loading';
 
-import styles from "./index.css"
+import styles from './index.css';
 
-const Page = (
-  {
-    isLoading,
-    __filename,
-    __url,
-    head,
-    body,
-    header,
-    footer,
-    children,
-  },
-  {
-    metadata: { pkg },
-  }
-) => {
-  warning(
-    typeof head.title === "string",
-    `Your page '${ __filename }' needs a title`
-  )
+class Page extends React.PureComponents {
+  render() {
+    const {
+      isLoading,
+      __filename,
+      __url,
+      head,
+      body,
+      header,
+      footer,
+      children,
+    } = this.props;
 
-  const metaTitle = head.metaTitle ? head.metaTitle : head.title
+    const {
+      metadata: { pkg },
+    } = this.context;
 
-  const socialImage = head.hero && head.hero.match("://") ? head.hero
-    : joinUri(process.env.PHENOMIC_USER_URL, head.hero)
+    warning(
+      typeof head.title === 'string',
+      `Your page '${__filename}' needs a title`,
+    );
 
-  const meta = [
-    { property: "og:type", content: "article" },
-    { property: "og:title", content: metaTitle },
-    {
-      property: "og:url",
-      content: joinUri(process.env.PHENOMIC_USER_URL, __url),
-    },
-    { property: "og:image", content: socialImage },
-    { property: "og:description", content: head.description },
-    { name: "twitter:card", content: "summary" },
-    { name: "twitter:title", content: metaTitle },
-    { name: "twitter:creator", content: `@${ pkg.twitter }` },
-    { name: "twitter:description", content: head.description },
-    { name: "twitter:image", content: socialImage },
-    { name: "description", content: head.description },
-  ]
+    const metaTitle = head.metaTitle ? head.metaTitle : head.title;
 
-  return (
-    <div className={ styles.page }>
-      <Helmet
-        title={ metaTitle }
-        meta={ meta }
-      />
+    const socialImage = head.hero && head.hero.match('://') ? head.hero
+    : joinUri(process.env.PHENOMIC_USER_URL, head.hero);
+
+    const meta = [
+    { property: 'og:type', content: 'article' },
+    { property: 'og:title', content: metaTitle },
       {
-        <div
-          className={ styles.hero }
-          style={ head.hero && {
-            background: `#111 url(${ head.hero }) 50% 50% / cover`,
-          } }
-        >
-          <div className={ styles.header }>
-            <div className={ styles.wrapper }>
-              <h1 className={ styles.heading }>{ head.title }</h1>
-              {
+        property: 'og:url',
+        content: joinUri(process.env.PHENOMIC_USER_URL, __url),
+      },
+    { property: 'og:image', content: socialImage },
+    { property: 'og:description', content: head.description },
+    { name: 'twitter:card', content: 'summary' },
+    { name: 'twitter:title', content: metaTitle },
+    { name: 'twitter:creator', content: `@${pkg.twitter}` },
+    { name: 'twitter:description', content: head.description },
+    { name: 'twitter:image', content: socialImage },
+    { name: 'description', content: head.description },
+    ];
+
+    return (
+      <div className={styles.page}>
+        <Helmet
+          title={metaTitle}
+          meta={meta}
+        />
+        {
+          <div
+            className={styles.hero}
+            style={head.hero && {
+              background: `#111 url(${head.hero}) 50% 50% / cover`,
+            }}
+          >
+            <div className={styles.header}>
+              <div className={styles.wrapper}>
+                <h1 className={styles.heading}>{ head.title }</h1>
+                {
                 head.cta &&
-                <Link to={ head.cta.link }>
-                  <Button className={ styles.cta } light { ...head.cta.props }>
+                <Link to={head.cta.link}>
+                  <Button className={styles.cta} light {...head.cta.props}>
                     { head.cta.label }
                   </Button>
                 </Link>
               }
+              </div>
             </div>
           </div>
-        </div>
       }
-      <div className={ styles.wrapper + " " + styles.pageContent }>
-        { header }
-        <div className={ styles.body }>
-          {
+        <div className={`${styles.wrapper} ${styles.pageContent}`}>
+          { header }
+          <div className={styles.body}>
+            {
             isLoading
             ? <Loading />
             : <BodyContainer>{ body }</BodyContainer>
           }
+          </div>
+          { children }
+          { footer }
         </div>
-        { children }
-        { footer }
       </div>
-    </div>
-  )
+    );
+  }
 }
 
 Page.propTypes = {
@@ -103,10 +106,10 @@ Page.propTypes = {
   body: PropTypes.string,
   header: PropTypes.element,
   footer: PropTypes.element,
-}
+};
 
 Page.contextTypes = {
   metadata: PropTypes.object.isRequired,
-}
+};
 
-export default Page
+export default Page;
